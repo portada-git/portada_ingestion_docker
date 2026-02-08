3ba0e7007c9a33a5920bba6a798b61d62c834ee4
jcanell4 <jcanell4@xtec.cat>
2026-02-08 20:00:50 +0100

    Parameter passing is made more flexible

--- dagster/definitions.py
+++ dagster/definitions.py
@@ -1,6 +1,6 @@
 from dagster import Definitions, load_assets_from_modules
 from dagster_portada_project.assets.boat_fact_ingestion_assets import ingestion
-from dagster_portada_project.resources.delta_data_layer_resource import DeltaDataLayerResource
+from dagster_portada_project.resources.delta_data_layer_resource import DeltaDataLayerResource, RedisConfig
 from dagster_portada_project.assets import boat_fact_ingestion_assets
 from dagster_portada_project.utilities import data_layer_builder_config_to_dagster_pyspark
 from dagster_pyspark import PySparkResource
@@ -22,7 +22,7 @@ defs = Definitions(
     resources={
         "py_spark_resource": py_spark_resource,
         "datalayer": DeltaDataLayerResource(py_spark_resource=py_spark_resource),
-        "redis_config":{"host":redis_host, "port":redis_port}
+        "redis_config": RedisConfig(host=redis_host, port=redis_port)
     },
     jobs=[ingestion]
 )
